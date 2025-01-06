af7364542d46c14653436fa8569b184383e9022c
Jeff Orchard <jorchard@uwaterloo.ca>
2025-01-06 12:36:32 -0500

    Removed parent folder from path for tests.

--- test_step.py
+++ test_step.py
@@ -1,8 +1,3 @@
-import sys
-import os
-# Add the main folder (parent directory) to sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import numpy as np
 import pytest
 import neuron_models as nm
