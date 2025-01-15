4355dc299e6d531d7cf009726540dd01d244498b
Jeff Orchard <jorchard@uwaterloo.ca>
2025-01-15 09:02:50 -0500

    slight change to avoid spike in test

--- tests/test_step.py
+++ tests/test_step.py
@@ -11,10 +11,10 @@ import ans as ans
 def test_step():
     A = ans.LIFNeuron(Tau_m=0.03, label='A')
     A.set_input_buffer(2.1)
-    A.set_v(-0.6)
+    A.set_v(-0.9)
     A.set_s(1.8)
     A.slope()
     A.step(0.02)
 
-    assert A._LIFNeuron__v == pytest.approx(1, abs=1e-4)
+    assert A._LIFNeuron__v == pytest.approx(0.99, abs=1e-4)
     assert A._LIFNeuron__s == pytest.approx(43.08, abs=1e-4)
